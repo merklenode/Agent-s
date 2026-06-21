1616937aa009aa1773e6a0c084651ec1ee63237e
sajjil akhtar <merklenode047@gmail.com>
2026-06-21 13:13:56 +0530

    remove oxe app

--- oxe/src/scripts/send-email.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "../lib/db";
-import { runWorkflow } from "../lib/workflow";
-import { sendMonthlyReportEmail } from "../tools/email.tool";
-
-runWorkflow("send:email", sendMonthlyReportEmail)
-  .then(() => { console.log("✓ Email sent"); })
-  .catch((e) => { console.error(e); process.exit(1); })
-  .finally(() => db.$disconnect());
